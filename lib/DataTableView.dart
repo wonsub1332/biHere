9eaa92b21d3e22cade30e713950bea84e61cabe0
wonsub1332 <wonsub5491@naver.com>
2023-05-23 17:41:08 +0900

    1.0.3

---
+++ lib/DataTableView.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+
+class DataTableView extends StatelessWidget {
+  final int data;
+
+  DataTableView(this.data);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: DataList(data),
+    );
+  }
+}
+
+class DataList extends StatelessWidget {
+  final int data;
+
+  DataList(this.data);
+
+  @override
+  Widget build(BuildContext context) {
+    return DataTable(
+      columns: const <DataColumn>[
+        DataColumn(
+          label: Expanded(
+            child: Text(
+              'Name',
+              style: TextStyle(fontStyle: FontStyle.italic),
+            ),
+          ),
+        ),
+        DataColumn(
+          label: Expanded(
+            child: Text(
+              'Age',
+              style: TextStyle(fontStyle: FontStyle.italic),
+            ),
+          ),
+        ),
+        DataColumn(
+          label: Expanded(
+            child: Text(
+              'Role',
+              style: TextStyle(fontStyle: FontStyle.italic),
+            ),
+          ),
+        ),
+      ],
+      rows: const <DataRow>[
+        DataRow(
+          cells: <DataCell>[
+            DataCell(Text('Sarah')),
+            DataCell(Text('19')),
+            DataCell(Text('Student')),
+          ],
+        ),
+        DataRow(
+          cells: <DataCell>[
+            DataCell(Text('Janine')),
+            DataCell(Text('43')),
+            DataCell(Text('Professor')),
+          ],
+        ),
+        DataRow(
+          cells: <DataCell>[
+            DataCell(Text('William')),
+            DataCell(Text('27')),
+            DataCell(Text('Associate Professor')),
+          ],
+        ),
+      ],
+    );
+  }
+}
